61e431831a59653ce15f5f59af922c724277086f
elnglr <61458490+elnglr@users.noreply.github.com>
2021-08-16 15:02:10 +0300

    Update dvdrental.sql

--- dvdrental.sql
+++ dvdrental.sql
@@ -63,3 +63,18 @@ SELECT MAX(length) FROM film WHERE rental_rate = 0.99
 
 SELECT COUNT( DISTINCT replacement_cost ) FROM film WHERE length > 150
 
+
+--odev 7#
+SELECT rating FROM film GROUP BY rating
+
+SELECT replacement_cost,COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50
+--or SELECT replacement_cost,COUNT(*) FROM film WHERE COUNT(*) > 50 GROUP BY replacement_cost 
+
+SELECT store_id,COUNT(*) FROM customer GROUP BY store_id
+
+SELECT country_id,COUNT(*) FROM city 
+GROUP BY country_id 
+ORDER BY COUNT(*) DESC 
+LIMIT 1;
+
+
