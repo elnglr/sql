966a7f3881d22a265b65aa26435277a070ee6ca8
elnglr <61458490+elnglr@users.noreply.github.com>
2021-08-18 16:20:59 +0300

    Update dvdrental.sql

--- dvdrental.sql
+++ dvdrental.sql
@@ -164,3 +164,12 @@ SELECT payment_id, first_name, last_name FROM customer INNER JOIN payment ON (pa
 
 SELECT first_name,last_name,rental_id FROM customer INNER JOIN rental ON( customer.customer_id = rental.customer_id );
 
+
+--hw 10 
+SELECT city,country FROM city LEFT JOIN country ON ( city.country_id = country.country_id );
+
+SELECT first_name,last_name,payment_id FROM customer RIGHT JOIN payment ON ( payment.customer_id = customer.customer_id);
+
+SELECT first_name,last_name,rental_id FROM customer FULL JOIN rental ON( customer.customer_id = rental.customer_id )
+
+
